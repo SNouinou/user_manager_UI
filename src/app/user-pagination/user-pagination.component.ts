edfbb1b27b97127ae6f2432efd6865aa205b3ce0
NOUINOU Saad <nouinou.saad@gmail.com>
2023-11-25 12:56:45 +0100

    user_pagination [feat] improve userPagination component to handle dynamic size of data.

--- src/app/user-pagination/user-pagination.component.ts
+++ src/app/user-pagination/user-pagination.component.ts
@@ -1,11 +1,62 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-user-pagination',
   templateUrl: './user-pagination.component.html',
-  styleUrls: ['./user-pagination.component.scss']
+  styleUrls: ['./user-pagination.component.scss'],
 })
-export class UserPaginationComponent {
+export class UserPaginationComponent implements OnChanges {
+  ngOnChanges(changes: SimpleChanges): void {
+    this.displayArray = [];
+    if (this.totalPages < 2 * this.size + 1) {
+      for (let page = 1; page <= this.totalPages; page++) {
+        this.display(page + '');
+      }
+    } else {
+      if (Math.abs(this.currentPage - 1) <= this.size + 2) {
+        for (let page = 1; page <= this.currentPage + this.size; page++) {
+          this.display(page + '');
+        }
+        this.display('...');
+        this.display(this.totalPages + '');
+      } else {
+        this.display('1');
+        this.display('...');
+        if (Math.abs(this.currentPage - this.totalPages) <= this.size + 2) {
+          for (
+            let page = this.currentPage - this.size;
+            page <= this.totalPages;
+            page++
+          ) {
+            this.display(page + '');
+          }
+        } else {
+          for (
+            let page = this.currentPage - this.size;
+            page <= this.currentPage + this.size;
+            page++
+          ) {
+            this.display(page + '');
+          }
+          this.display('...');
+          this.display(this.totalPages + '');
+        }
+      }
+    }
+  }
+
+  size = 2;
+  displayArray: string[] = [];
+  _Number = Number;
+
   @Input() totalPages: number = 1;
   @Input() currentPage: number = 1;
 
@@ -15,4 +66,7 @@ export class UserPaginationComponent {
     this.fetchUserPage.emit(page);
   }
 
+  display(pageButton: string) {
+    this.displayArray.push(pageButton);
+  }
 }
